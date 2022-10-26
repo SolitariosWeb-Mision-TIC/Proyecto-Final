ba5d20a5e42282321543ea06f3f7b73731ea3176
Deyproj <deyproj@hotmail.com>
2022-10-26 10:50:04 -0500

    mejora carrito y botones

--- global-mobile/src/views/Products/productsClient.js
+++ global-mobile/src/views/Products/productsClient.js
@@ -1,53 +1,58 @@
 import React, { useState } from "react";
 
 const productos = [
-  { id: 1, name: "Xiaomi", descripcion: "Test 1" },
-  { id: 2, name: "Samsung", descripcion: "Test 2" },
-  { id: 3, name: "Motorola", descripcion: "Test 3" },
-  { id: 4, name: "Huwei", descripcion: "Test 3" },
-  { id: 5, name: "Oppo", descripcion: "Test 3" },
-  { id: 6, name: "Xperia", descripcion: "Test 3" },
-  { id: 7, name: "Redmi 9", descripcion: "Test 3" },
-  { id: 8, name: "Redmi 10", descripcion: "Test 3" },
-  { id: 9, name: "Xiaomi2", descripcion: "Test 1" },
-  { id: 10, name: "Xiaomi3", descripcion: "Test 1" },
-  { id: 11, name: "Xiaomi4", descripcion: "Test 1" },
-  { id: 12, name: "Samsung", descripcion: "Test 2" },
-  { id: 13, name: "Motorola", descripcion: "Test 3" },
-  { id: 14, name: "Huwei", descripcion: "Test 3" },
-  { id: 15, name: "Oppo", descripcion: "Test 3" },
-  { id: 16, name: "Xperia", descripcion: "Test 3" },
-  { id: 17, name: "Redmi 9", descripcion: "Test 3" },
-  { id: 18, name: "Redmi 10", descripcion: "Test 3" },
+  { id: 1, name: "Xiaomi", descripcion: "Test 1", valor: 500000 },
+  { id: 2, name: "Samsung", descripcion: "Test 2", valor: 600000 },
+  { id: 3, name: "Motorola", descripcion: "Test 3", valor: 800000 },
+  { id: 4, name: "Huwei", descripcion: "Test 3", valor: 450000 },
+  { id: 5, name: "Oppo", descripcion: "Test 3", valor: 550000 },
+  { id: 6, name: "Xperia", descripcion: "Test 3", valor: 620000 },
+  { id: 7, name: "Redmi 9", descripcion: "Test 3", valor: 580000 },
+  { id: 8, name: "Redmi 10", descripcion: "Test 3", valor: 570000 },
+  { id: 9, name: "Xiaomi2", descripcion: "Test 1", valor: 620000 },
+  { id: 10, name: "Xiaomi3", descripcion: "Test 1", valor: 300000 },
+  { id: 11, name: "Xiaomi4", descripcion: "Test 1", valor: 350000 },
+  { id: 12, name: "Samsung", descripcion: "Test 2", valor: 590000 },
+  { id: 13, name: "Motorola", descripcion: "Test 3", valor: 800000 },
+  { id: 14, name: "Huwei", descripcion: "Test 3", valor: 560000 },
+  { id: 15, name: "Oppo", descripcion: "Test 3", valor: 720000 },
+  { id: 16, name: "Xperia", descripcion: "Test 3", valor: 6500000 },
+  { id: 17, name: "Redmi 9", descripcion: "Test 3", valor: 4200000 },
+  { id: 18, name: "Redmi 10", descripcion: "Test 3", valor: 810000 },
 ];
 
 const ProductsClient = () => {
   const [carrito, setCarrito] = useState([]);
 
   const agregarCarrito = (producto) => {
-    console.log(producto.name);
     setCarrito([...carrito, producto]);
   };
 
-  const cambiarBoton = (oprimido) => {
-    if (oprimido) {
-      var className = 'btn';
-      className += 'btn-primary';
-    } else {
-      className += 'btn-danger';
-    }
-  }
-
-  const [oprimido, setOprimido] = useState(false);
   const oprimio = (producto) => {
-    agregarCarrito(producto)
-    setOprimido(!oprimido)
-    cambiarBoton(oprimido)
-  }
+    if (carrito.includes(producto)) {
+      setCarrito(carrito.filter((item) => item !== producto));
+    } else {
+      agregarCarrito(producto);
+    }
+  };
+
+  const totalCarro = (carrito) => {
+    let total = 0;
+    carrito.forEach((element) => {
+      total += element.valor;
+    });
+    return total;
+  };
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  });
 
   return (
     <>
-      <h1 className="text-center mt-3">Productos</h1>
+      <h1 className="text-center mt-3 text-muted">Productos en venta</h1>
       <br />
       <div className="container">
         <div className="row">
@@ -55,8 +60,8 @@ const ProductsClient = () => {
             <div className="row">
               {productos.map((producto, index) => {
                 return (
-                  <div className="col-3">
-                    <div className="card mb-4" key={index}>
+                  <div key={producto.id} className="col-3">
+                    <div className="card mb-4 shadow-sm">
                       <img
                         className="card-img-top"
                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuZVbcygH3QZNKGOOqgAN9WRQedPmX_8LpjA&usqp=CAU"
@@ -69,14 +74,23 @@ const ProductsClient = () => {
                         <p className="card-text text-center">
                           {producto.descripcion}
                         </p>
+                        <p className="card-text text-center">
+                          {formatter.format(producto.valor)}
+                        </p>
                         <div className="text-center">
                           <a
                             href="#a"
-                            class="btn btn-primary"
+                            className={
+                              carrito.includes(producto)
+                                ? "btn btn-danger"
+                                : "btn btn-primary"
+                            }
                             name="elemento"
                             onClick={() => oprimio(producto)}
                           >
-                            {oprimido ? "Agregar" : "Eliminar"}
+                            {carrito.includes(producto)
+                              ? "Eliminar"
+                              : "Agregar"}
                           </a>
                         </div>
                       </div>
@@ -86,14 +100,33 @@ const ProductsClient = () => {
               })}
             </div>
           </div>
-          <div class="col-3 card border-secondary mb-3">
-            <div class="card-body text-secondary text-center">
-              <h5 class="card-title">Carrito</h5>
-              <p class="card-text">
-                {carrito.map((productCart) => {
-                  return <p>{productCart.name}</p>;
-                })}
-              </p>
+          <div className="col-3">
+            <div className="card border-secondary mb-3 rounded shadow-lg">
+              <div className="card-body text-secondary text-center">
+                <h5 className="card-title">Carrito</h5>
+                <table class="table card-text">
+                  <thead>
+                    <tr className="text-muted">
+                      <th scope="col">Nombre</th>
+                      <th scope="col">Valor</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {carrito.map((productCart) => {
+                      return (
+                        <tr className="card-text">
+                          <td>{productCart.name}</td>
+                          <td>{formatter.format(productCart.valor)}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+
+                <p className="card-footer">
+                  Total {formatter.format(totalCarro(carrito))}
+                </p>
+              </div>
             </div>
           </div>
         </div>
