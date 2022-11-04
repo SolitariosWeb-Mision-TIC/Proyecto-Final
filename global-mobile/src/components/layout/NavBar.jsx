8ebd36e7f0faf0719d0687e82653a51a5da1ba53
Deyproj <41933171+Deyproj@users.noreply.github.com>
2022-11-03 22:02:26 -0500

    Router

---
+++ global-mobile/src/components/layout/NavBar.jsx
@@ -0,0 +1,40 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="navBar " variant="dark">
+      <Container>
+        <Navbar.Brand>Global Mobile</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link eventKey={1}>
+              <Link to="/create" className="link">
+                Creacion Productos
+              </Link>
+            </Nav.Link>
+            <Nav.Link eventKey={2}>
+              <Link to="/" className="link">
+                Productos en venta
+              </Link>
+            </Nav.Link>
+            <Nav.Link eventKey={3}>
+              <Link to="/ventas" className="link">
+                Lista de ventas
+              </Link>
+            </Nav.Link>
+            <Nav.Link eventKey={4} href="#">
+              Salir
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
